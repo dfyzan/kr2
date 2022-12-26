1f063e2b3e43a5ad65345d997a28b7b9f471da24
Ghost2021 <rtmuskov@gmail.com>
2022-12-26 18:03:53 +0300

    1

---
+++ cmake-build-debug/CMakeFiles/kr2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kr2.dir/main.c.obj"
+  "CMakeFiles/kr2.dir/main.c.obj.d"
+  "kr2.exe"
+  "kr2.exe.manifest"
+  "kr2.pdb"
+  "libkr2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kr2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/kr2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kr2.
+# This may be replaced when dependencies are built.
